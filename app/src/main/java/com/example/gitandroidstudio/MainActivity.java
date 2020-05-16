b0698e004f6fe48b64985acf266ecf239acecf76
vidyashri <vidyashrikalshetti@gmail.com>
2020-05-16 11:40:06 +0530

    my 1st msg

--- app/src/main/java/com/example/gitandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/gitandroidstudio/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("TAG","Message");
         Log.i("TAG","Message");
+        Log.i("TAG","Message");
     }
 }
